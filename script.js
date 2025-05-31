4b2681011e2e0b977bc7ca15742f77740a195379
kirfor <94166912+kirfor@users.noreply.github.com>
2025-06-01 00:13:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Сайт загружен!");
 
 // Пример интерактивности
 document.querySelector('h1').addEventListener('click', () => {
-    alert('Добро пожаловать на мой сайт!!!!!!!!!!');
+    alert('Добро пожаловать на мой сайт!');
 });
 document.querySelector('h2').addEventListener('click', () => {
     alert('О проекте');
